d36d728a4f40661d7c57b10c31556a8a56daa0a8
Elben Shira <elbenshira@gmail.com>
2011-09-15 20:40:48 -0500

    Remove unneeded pushTweet2 function.

--- js/application.js
+++ js/application.js
@@ -50,24 +50,6 @@ $(function () {
         $(".stream").append(item);
     };
 
-    var pushTweet2 = function (tweet) {
-        var html = $('\
-                <div class="item">\
-                <div class="pic"><img></div>\
-                <div class="right-content">\
-                <div class="author"><span class="screen-name"></span> <span class="name"></span></div>\
-                <div class="text">\
-                </div>\
-                </div>\
-                </div>');
-
-        html.select(".author .screen-name").first().html(tweet.user.screen_name);
-        //html.select(".author .name").first().html(tweet.user.name);
-        //html.select(".text").first().html(tweet.text);
-
-        $(".stream").append(html);
-    };
-
     var receiveTweets = function (tweets) {
         $.each(tweets, function(i, tweet) {
             pushTweet(tweet);

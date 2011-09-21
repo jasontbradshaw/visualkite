7b11032ff5628fb93a8ca9305cdd049793d12e89
Elben Shira <elbenshira@gmail.com>
2011-09-21 18:40:24 -0500

    Work on StoredQueue and LocalStorage.
    - Tests for StoredQueue and LocalStorage.

--- js-tests/carousel_spec.js
+++ js-tests/carousel_spec.js
@@ -65,31 +65,105 @@ describe("Carousel", function () {
   });
 });
 
-describe("StoredQueue", function () {
-  var storedQueue;
+describe("LocalStorage", function () {
+  var storage;
 
   beforeEach(function () {
-    storedQueue = new StoredQueue('test');
+    storage = new LocalStorage('my.test');
   });
 
-  describe("key", function () {
+  afterEach(function () {
+    storage.clear();
+  });
+
+  describe("_key", function () {
     it("should build key properly", function () {
-      expect(storedQueue._key('myattribute')).toEqual('StoredQueue.test.myattribute');
-      expect(storedQueue._key('')).toEqual('StoredQueue.test');
-      expect(storedQueue._key(null)).toEqual('StoredQueue.test');
-      expect(storedQueue._key(undefined)).toEqual('StoredQueue.test');
+      expect(storage._key('myattribute')).toEqual('LocalStorage.my.test.myattribute');
+      expect(storage._key('')).toEqual('LocalStorage.my.test');
+      expect(storage._key(null)).toEqual('LocalStorage.my.test');
+      expect(storage._key(undefined)).toEqual('LocalStorage.my.test');
+      expect(storage._key()).toEqual('LocalStorage.my.test');
     });
+  });
+
+  describe("get", function () {
+    it("should return undefined if attribute does not exist", function () {
+      expect(storage.get('myattribute')).toEqual(undefined);
+    });
+
+    it("should return value if it exists", function () {
+      storage.set('myattribute', 'my data');
+      expect(storage.get('myattribute')).toEqual("my data");
+    });
+
+    it("should return value as a string", function () {
+      storage.set('myattribute', {id: 32});
+      expect(storage.get('myattribute')).toEqual('{"id":32}');
+    });
+  });
+
+  describe("json", function () {
+    it("should return undefined if attribute does not exist", function () {
+      expect(storage.json('myattribute')).toEqual(undefined);
+    });
+
+    it("should return as JSON", function () {
+      storage.set('myattribute', {id: 32, names: ['elben', 'jason', 'paul']});
+      expect(storage.json('myattribute')).toEqual({id :32, names: ['elben', 'jason', 'paul']});
+    });
+
+    it("should return as array", function () {
+      storage.set('myattribute', ['yep', 32]);
+      expect(storage.json('myattribute')).toEqual(['yep', 32]);
+    });
+  });
+
+  describe("int", function () {
+    it("should return undefined if attribute does not exist", function () {
+      expect(storage.json('myattribute')).toEqual(undefined);
+    });
+
+    it("should return as int", function () {
+      storage.set('myattribute', 32);
+      expect(storage.int('myattribute')).toEqual(32);
+    });
+  });
+
+  describe("incr", function () {
+    it("should return undefined if attribute does not exist", function () {
+      expect(storage.json('myattribute')).toEqual(undefined);
+    });
+
+    it("should increment floats", function () {
+      storage.set('myattribute', 32.2);
+      expect(storage.incr('myattribute')).toEqual(33.2);
+      expect(storage.get('myattribute')).toEqual('33.2');
+    });
+
+    it("should increment integers", function () {
+      storage.set('myattribute', 32);
+      expect(storage.incr('myattribute')).toEqual(33);
+      expect(storage.get('myattribute')).toEqual('33');
+    });
+  });
+});
+
+describe("StoredQueue", function () {
+  var storage;
+
+  beforeEach(function () {
+    storage = new StoredQueue("myqueue");
   });
 
   describe("length", function () {
     it("should be zero if empty", function () {
-      expect(storedQueue.length()).toEqual(0);
+      expect(storage.length()).toEqual(0);
     });
 
     it("should be the right size", function () {
-      storedQueue.push({id: 1});
-      storedQueue.push({id: 2});
-      expect(storedQueue.length()).toEqual(2);
+      storage.push({id: 1});
+      storage.push({id: 2});
+      expect(storage.length()).toEqual(2);
     });
   });
 });
--- js/carousel.js
+++ js/carousel.js
@@ -35,48 +35,111 @@ function Carousel() {
 };
 
 
-// Queue implementation using local storage.
+// Queue implementation using LocalStorage.
 
 function StoredQueue(name) {
-  window.localStorage["StoredQueue." + name + ".length"] = 0;
+  var storage = new LocalStorage('StoredQueue.' + name);
+  storage.set('queue', []);
+
+  this.queue = function () {
+    return storage.json('queue');
+  };
+
+  this.length = function () {
+    return this.queue().length;
+  };
+
+  this.push = function (item) {
+    var queue = this.queue();
+    queue.push(item);
+    storage.set('queue', queue);
+  };
+
+  this.pop = function () {
+    // TODO this is inefficient because shift() is O(n). We can do amortized
+    // O(1) using offsets. Check out Queue.js.
+    var queue = this.queue();
+    var item = queue.shift();
+    storage.set('queue', queue);
+    return item;
+  };
+}
+
+// LocalStorage is a wrapper around window.localStorage.
+//
+// Requires json2.js.
+
+function LocalStorage(name) {
 
   this._key = function (attribute) {
     if(attribute && attribute != '') {
-      return "StoredQueue." + name + "." + attribute;
+      return "LocalStorage." + name + "." + attribute;
     }
-    return "StoredQueue." + name;
+    return "LocalStorage." + name;
   };
 
-  this._get = function (attribute) {
+  this.get = function (attribute) {
     var key = this._key(attribute);
     return window.localStorage[key];
   };
 
-  this._set = function (attribute, value) {
+  this.set = function (attribute, value) {
+    if(typeof(value) != "string") {
+      value = JSON.stringify(value);
+    }
     var key = this._key(attribute);
     window.localStorage[key] = value;
   };
 
-  this._incr = function (attribute) {
-    var value = parseInt(this._get(attribute));
-    this._set(attribute, value+1);
-  };
-
-  this._json = function (attribute) {
-    return JSON.parse(this._get(attribute));
+  this.json = function (attribute) {
+    var value = this.get(attribute);
+    if(!value) return undefined;
+    return JSON.parse(value);
   }
 
-  this.length = function () {
-    return parseInt(this._get('length'));
+  this.int = function (attribute) {
+    var value = this.get(attribute);
+    if(!value) return undefined;
+    return parseInt(value);
+  }
+
+  this.incr = function (attribute, incrValue) {
+    if(typeof incrValue == "undefined") {
+      incrValue = 1;
+    }
+
+    var value = this.get(attribute);
+    if(!value) return undefined;
+
+    value = parseFloat(this.get(attribute));
+    this.set(attribute, value+incrValue);
+    return value + incrValue;
   };
 
-  this.push = function (item) {
-    this._incr('length');
-    var queue = this._json('queue');
-    // TODO
+  this.clear = function (attribute) {
+    var len = this._key().length;
+    var namespace = this._key();
+    var deletedKeys = [];
+
+    for(var key in window.localStorage) {
+      if(window.localStorage.hasOwnProperty(key)) {
+        var substr = key.substring(0, len);
+        if(substr == namespace) {
+          deletedKeys.push(key);
+          delete window.localStorage[key];
+        }
+      }
+    }
+    return deletedKeys;
   };
 
-  this.pop = function () {
-    // TODO make it cool like Queue.js
+  this._allLocalStorageKeys = function () {
+    var keys = [];
+    for(var key in window.localStorage) {
+      if(window.localStorage.hasOwnProperty(key)) {
+        keys.push(key);
+      }
+    }
+    return keys;
   };
 }
